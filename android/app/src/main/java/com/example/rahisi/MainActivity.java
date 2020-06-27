b3904a91c29a46e57740227ea75bc85bc2c05293
leonkoech <leonkipkip@gmail.com>
2020-06-27 18:00:54 +0300

    initial commit

---
+++ android/app/src/main/java/com/example/rahisi/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.rahisi;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+
+public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "com.example.rahisi/epic";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            // Note: this method is invoked on the main thread.
+                            // TODO
+                            if (call.method.equals("printy")) {
+//                                result.success("hi from j");
+
+//                            run the fuction that opens mpesa
+                            }
+                        }
+                );
+    }
+
+
+
+
+}
